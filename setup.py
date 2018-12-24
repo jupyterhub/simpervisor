e47b7fbbe9debb892cf9eac4d80b3d7d73baa5b3
yuvipanda <yuvipanda@gmail.com>
2018-12-24 13:07:06 -0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="simpervisor",
+    version='0.1',
+    url="https://github.com/yuvipanda/simpervisor",
+    author="Yuvi Panda",
+    author_email="yuvipanda@gmail.com",
+    license="BSD 3-Clause",
+    packages=setuptools.find_packages(),
+)
