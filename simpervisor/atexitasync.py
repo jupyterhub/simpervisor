bbc3d9e8b6f71f72ac0b2a7cce15e5a3dfc18230
yuvipanda <yuvipanda@gmail.com>
2018-12-26 12:16:32 -0800

    Don't add signal handler multiple times

--- simpervisor/atexitasync.py
+++ simpervisor/atexitasync.py
@@ -12,10 +12,12 @@ _handlers = []
 signal_handler_set = False
 
 def add_handler(handler):
+    global signal_handler_set
     if not signal_handler_set:
         loop = asyncio.get_event_loop()
         loop.add_signal_handler(signal.SIGINT, _handle_signal, signal.SIGINT)
         loop.add_signal_handler(signal.SIGTERM, _handle_signal, signal.SIGTERM)
+        signal_handler_set = True
     _handlers.append(handler)
 
 def remove_handler(handler):
