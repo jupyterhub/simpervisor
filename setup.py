058e55ecef04daa0d907f78f27e81a5705f21afc
yuvipanda <yuvipanda@gmail.com>
2018-12-28 13:16:35 -0800

    Add description to setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setuptools.setup(
     name="simpervisor",
     version='0.1',
     url="https://github.com/yuvipanda/simpervisor",
+    description='Simple async process supervisor',
     author="Yuvi Panda",
     author_email="yuvipanda@gmail.com",
     license="BSD 3-Clause",
