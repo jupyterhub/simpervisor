b736f117f30ab59db7fbd2ae7637b9fdf6d0295d
yuvipanda <yuvipanda@gmail.com>
2019-01-04 13:40:21 -0800

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="simpervisor",
-    version='0.1',
+    version='0.2',
     url="https://github.com/yuvipanda/simpervisor",
     description='Simple async process supervisor',
     author="Yuvi Panda",
