5999f601866bca64c5ceca9cd37c14710c033017
mahendrapaipuri <mahendra.paipuri@gmail.com>
2023-06-27 15:44:18 +0200

    Take existing signal handlers into account
    Ensure that we do not override the existing signal handlers registered
    in Python

--- simpervisor/atexitasync.py
+++ simpervisor/atexitasync.py
@@ -7,6 +7,7 @@ import signal
 import sys
 
 _handlers = []
+_existing_handlers = {}
 
 signal_handler_set = False
 
@@ -14,6 +15,14 @@ signal_handler_set = False
 def add_handler(handler):
     global signal_handler_set
     if not signal_handler_set:
+        # First get existing signal handlers for SIGINT and SIGTERM
+        # If no handlers are defined in Python, getsignal will return None
+        _existing_handlers.update(
+            {
+                signal.SIGINT: signal.getsignal(signal.SIGINT),
+                signal.SIGTERM: signal.getsignal(signal.SIGTERM),
+            }
+        )
         signal.signal(signal.SIGINT, _handle_signal)
         signal.signal(signal.SIGTERM, _handle_signal)
         signal_handler_set = True
@@ -31,4 +40,9 @@ def _handle_signal(signum, *args):
         signum = signal.CTRL_C_EVENT
     for handler in _handlers:
         handler(signum)
-    sys.exit(0)
+    # Finally execute any existing handler that were registered in Python
+    existing_handler = _existing_handlers.get(signum, None)
+    if existing_handler is not None:
+        existing_handler(signum, None)
+    else:
+        sys.exit(0)
