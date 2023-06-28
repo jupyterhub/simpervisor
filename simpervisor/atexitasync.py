b52fd8d6a4392673dc67b6b75574967dcb8d6a05
mahendrapaipuri <mahendra.paipuri@gmail.com>
2023-06-28 12:01:26 +0200

    Ensure existing handlers are not default ones
    We check if the existing handlers for SIGINT and SIGTERM are different
    from default handlers.
    
    Only when they are custom handlers, we execute them in our custom
    handler

--- simpervisor/atexitasync.py
+++ simpervisor/atexitasync.py
@@ -15,18 +15,31 @@ signal_handler_set = False
 def add_handler(handler):
     global signal_handler_set
     if not signal_handler_set:
-        # First get existing signal handlers for SIGINT and SIGTERM
-        # If no handlers are defined in Python, getsignal will return None
+        # Add handler of SIGINT only if it is not default
+        _existing_sigint_handler = signal.getsignal(signal.SIGINT)
+        if _existing_sigint_handler.__qualname__ == 'default_int_handler':
+            _existing_sigint_handler = None
         _existing_handlers.update(
             {
-                signal.SIGINT: signal.getsignal(signal.SIGINT),
-                signal.SIGTERM: signal.getsignal(signal.SIGTERM),
+                signal.SIGINT: _existing_sigint_handler,
             }
         )
+
+        # Add handler of SIGTERM only if it is not default
+        _existing_sigterm_handler = signal.getsignal(signal.SIGTERM)
+        if _existing_sigterm_handler == signal.Handlers.SIG_DFL:
+            _existing_sigterm_handler = None
+        _existing_handlers.update(
+            {
+                signal.SIGTERM: _existing_sigterm_handler,
+            }
+        )
+
         signal.signal(signal.SIGINT, _handle_signal)
         signal.signal(signal.SIGTERM, _handle_signal)
         signal_handler_set = True
     _handlers.append(handler)
+    # print(_existing_handlers)
 
 
 def remove_handler(handler):
@@ -41,8 +54,9 @@ def _handle_signal(signum, *args):
     for handler in _handlers:
         handler(signum)
     # Finally execute any existing handler that were registered in Python
+    # Bail if existing handler is not a callable
     existing_handler = _existing_handlers.get(signum, None)
-    if existing_handler is not None:
+    if existing_handler is not None and callable(existing_handler):
         existing_handler(signum, None)
     else:
         sys.exit(0)
