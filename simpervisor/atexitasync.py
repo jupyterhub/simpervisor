15304ee0c91dc83408ee3d5cc2dcc5d5fed690e3
Erik Sundell <erik.i.sundell@gmail.com>
2024-01-30 09:33:13 +0100

    Refactor for readability

--- simpervisor/atexitasync.py
+++ simpervisor/atexitasync.py
@@ -7,55 +7,65 @@ import signal
 import sys
 
 _handlers = []
-_existing_handlers = {}
 
+_prev_handlers = {}
 signal_handler_set = False
 
 
 def add_handler(handler):
-    global signal_handler_set
-    if not signal_handler_set:
-        # Add handler of SIGINT only if it is not default
-        _existing_sigint_handler = signal.getsignal(signal.SIGINT)
-        if _existing_sigint_handler.__qualname__ == "default_int_handler":
-            _existing_sigint_handler = None
-        _existing_handlers.update(
-            {
-                signal.SIGINT: _existing_sigint_handler,
-            }
-        )
-
-        # Add handler of SIGTERM only if it is not default
-        _existing_sigterm_handler = signal.getsignal(signal.SIGTERM)
-        if _existing_sigterm_handler == signal.Handlers.SIG_DFL:
-            _existing_sigterm_handler = None
-        _existing_handlers.update(
-            {
-                signal.SIGTERM: _existing_sigterm_handler,
-            }
-        )
-
-        signal.signal(signal.SIGINT, _handle_signal)
-        signal.signal(signal.SIGTERM, _handle_signal)
-        signal_handler_set = True
+    """
+    Adds a signal handler function that will be called when the Python process
+    receives either a SIGINT (on windows CTRL_C_EVENT) or SIGTERM signal.
+    """
+    _ensure_signal_handlers_set()
     _handlers.append(handler)
 
 
 def remove_handler(handler):
+    """Removes previously added signal handler."""
     _handlers.remove(handler)
 
 
+def _ensure_signal_handlers_set():
+    """
+    Ensures _handle_signal is registered as a top level signal handler for
+    SIGINT and SIGTERM and saves previously registered non-default Python
+    callable signal handlers.
+    """
+    global signal_handler_set
+    if not signal_handler_set:
+        # save previously registered non-default Python callable signal handlers
+        prev_sigint = signal.getsignal(signal.SIGINT)
+        prev_sigterm = signal.getsignal(signal.SIGTERM)
+        if callable(prev_sigint) and prev_sigint.__qualname__ != "default_int_handler":
+            _prev_handlers[signal.SIGINT] = prev_sigint
+        if callable(prev_sigterm) and prev_sigterm != signal.Handlers.SIG_DFL:
+            _prev_handlers[signal.SIGTERM] = prev_sigint
+
+        # let _handle_signal handle SIGINT and SIGTERM
+        signal.signal(signal.SIGINT, _handle_signal)
+        signal.signal(signal.SIGTERM, _handle_signal)
+        signal_handler_set = True
+
+
 def _handle_signal(signum, *args):
+    """
+    Calls functions added by add_handler, and then calls the previously
+    registered non-default Python callable signal handler if there were one.
+    """
     # Windows doesn't support SIGINT. Replacing it with CTRL_C_EVENT so that it
     # can used with subprocess.Popen.send_signal
     if signum == signal.SIGINT and sys.platform == "win32":
         signum = signal.CTRL_C_EVENT
+
     for handler in _handlers:
         handler(signum)
-    # Finally execute any existing handler that were registered in Python
-    # Bail if existing handler is not a callable
-    existing_handler = _existing_handlers.get(signum, None)
-    if existing_handler is not None and callable(existing_handler):
-        existing_handler(signum, None)
+
+    # call previously registered non-default Python callable handler or exit
+    # FIXME: This is currently incorrectly implemented for windows as there are
+    #        no _prev_handlers entry for signal.CTRL_C_EVENT!
+    prev_handler = _prev_handlers.get(signum)
+    if prev_handler:
+        prev_handler(signum, None)
     else:
         sys.exit(0)
